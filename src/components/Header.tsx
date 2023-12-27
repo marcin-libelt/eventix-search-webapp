c865c8a1203db63d8f66efc8d9ce558cedebbbb3
Marcin Libelt <marcin.libelt@gmail.com>
2023-12-27 13:01:00 +0100

    Header and Footer boostrap

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <Link href="/">ss</Link>
+      <Link href="/events/all">All Events</Link>
+    </header>
+  );
+}
