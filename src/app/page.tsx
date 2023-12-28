07db9a5aada8cb1b3206a95ebbbfd8667e55bf95
Marcin Libelt <marcin.libelt@gmail.com>
2023-12-28 14:31:00 +0100

    general styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Finfd events around you</h1>
-      <p>Browse more than 10,000 events around you</p>
+    <main className="flex flex-col items-center pt-36 px-3">
+      <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
+        Find events around you
+      </h1>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl">
+        Browse more than{" "}
+        <span className="font-bold italic underline text-[#a4f839]">
+          10,000 events
+        </span>{" "}
+        around you
+      </p>
 
-      <form>
+      <form className="w-full sm:w-[580px]">
         <input
           type="text"
           spellCheck={false}
           placeholder="Search events in any city..."
+          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 focus:bg-white/10 focus:ring-2 transition"
         />
       </form>
 
-      <section>
-        <div>
+      <section className="flex mt-4 gap-x-4 text-sm text-white/50">
+        <div className="space-x-2 font-semibold">
           <Link href="/events/austin">Austin</Link>
           <Link href="/events/seatle">Seatle</Link>
         </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,30 @@
+import Link from "next/link";
 import React from "react";
 
+const routes = [
+  {
+    path: "/terms-condidtions",
+    name: "Terms & Conditions",
+  },
+  {
+    path: "/privacy-policy",
+    name: "Privacy Policy",
+  },
+];
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="mt-auto flex items-center justify-between h-16 border-t border-white/10 px-3 md:px-9 text-xs text-white/25">
+      <small className="text-xs">
+        &copy; 2024 Lumost It. ALl rights reserved.
+      </small>
+      <ul className="flex gap-x-3 sm:gap-x-8">
+        {routes.map(({ path, name }) => (
+          <li key={path}>
+            <Link href={path}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 }
