fec32b11212cfeb8083ad5487e3686792bfc0cfd
Marcin Libelt <marcin.libelt@gmail.com>
2023-12-28 11:53:00 +0100

    styling header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,35 @@ import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
 
+const routeLinks = [
+  {
+    path: "/",
+    name: "Home",
+  },
+  {
+    path: "/events/all",
+    name: "All events",
+  },
+];
+
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 md:px-9 ">
       <Logo />
-      <Link href="/">ss</Link>
-      <Link href="/events/all">All Events</Link>
+      <nav>
+        <ul className="flex gap-x-6 text-sm">
+          {routeLinks.map(({ path, name }) => (
+            <li
+              key={path}
+              className="text-white/50 hover:text-white transition"
+            >
+              <Link href={path} className="">
+                {name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
