8d0fe921e6eceffd2b034238363e9ce9a0a5e51a
Marcin Libelt <marcin.libelt@gmail.com>
2024-01-03 12:59:00 +0100

    moved H1 to reausable component, add link to logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Image
-      src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
-      alt=""
-      width="53"
-      height="12"
-    />
+    <Link href="/">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
+        alt=""
+        width="53"
+        height="12"
+      />
+    </Link>
   );
 }
