18a8103a08ff5b2c21fadc5db5c648905825075d
Marcin Libelt <marcin.libelt@gmail.com>
2024-01-04 09:55:00 +0100

    created types file, added fecth request for events in austin - hardcoded for now, css styles for scrollbar, scrollbar always visible

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,32 @@
 import H1 from "@/components/H1";
+import { EventoEvent } from "@/types";
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default async function EventsPage({ params }: EventsPageProps) {
+  const city = params.city;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=austin`
+  );
+
+  const events: EventoEvent[] = await response.json();
+
   return (
-    <main>
-      <H1>Events in Austin</H1>
+    <main className="flex flex-col items-center py-24 px-5 min-h-28">
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   );
 }
