ceeb1c29767c24d60bb73ed5c1738ddbe840c9b6
Marcin Libelt <marcin.libelt@gmail.com>
2024-01-12 11:58:00 +0100

    prevent usage server utilities on client component by installing 'server-only' plugin

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -1,40 +1,44 @@
+import "server-only";
 import { notFound } from "next/navigation";
 import { EventoEvent, PrismaClient } from "@prisma/client";
 import { capitalize } from "./utils";
+import { unstable_cache } from "next/cache";
 
 const prisma = new PrismaClient();
 const PAGE_SIZE = 6;
 
-export async function getEvents(city: string, page: number = 1) {
-  const events = await prisma.eventoEvent.findMany({
-    where: {
-      city: city === "all" ? undefined : capitalize(city),
-    },
-    orderBy: {
-      date: "asc",
-    },
-    take: PAGE_SIZE,
-    skip: (page - 1) * PAGE_SIZE,
-  });
-
-  let totalCount;
-  if (city === "all") {
-    totalCount = await prisma.eventoEvent.count();
-  } else {
-    totalCount = await prisma.eventoEvent.count({
+export const getEvents = unstable_cache(
+  async (city: string, page: number = 1) => {
+    const events = await prisma.eventoEvent.findMany({
       where: {
-        city: capitalize(city),
+        city: city === "all" ? undefined : capitalize(city),
       },
+      orderBy: {
+        date: "asc",
+      },
+      take: PAGE_SIZE,
+      skip: (page - 1) * PAGE_SIZE,
     });
+
+    let totalCount;
+    if (city === "all") {
+      totalCount = await prisma.eventoEvent.count();
+    } else {
+      totalCount = await prisma.eventoEvent.count({
+        where: {
+          city: capitalize(city),
+        },
+      });
+    }
+
+    return {
+      events,
+      totalCount,
+    };
   }
+);
 
-  return {
-    events,
-    totalCount,
-  };
-}
-
-export async function getEvent(slug: string) {
+export const getEvent = unstable_cache(async (slug: string) => {
   const event = await prisma.eventoEvent.findUnique({
     where: {
       slug,
@@ -42,4 +46,4 @@ export async function getEvent(slug: string) {
   });
   if (!event) return notFound();
   return event;
-}
+});
